471dc76bf7620232dd4dbd6c1b35836e526f0401
benboussad mehdi-A <areclen2000@hotmail.fr>
2024-05-16 16:40:58 +0100

    code updated

--- barend.py
+++ barend.py
@@ -1,27 +1,19 @@
-import barcode
+
 from barcode import Code128
 from barcode.writer import ImageWriter
 
-def generate_barcode(data, filename):
-    # Generate Code 128 barcode
+def pambcgen(data, fish):
     code128 = Code128(data, writer=ImageWriter())
-
-    # Save the barcode image
-    filename_with_extension = f"{filename}"
-    code128.save(filename_with_extension)
+    filexten = f"{fish}"
+    code128.save(filexten)
 
 if __name__ == "__main__":
-    # Define variables
-    fac_num = "1608"    # Facture number (4 digits)
-    lot = "ebi233"      # Lot number (6 characters)
-    exp_date = "0124"   # Expiry date (4 digits)
-    price = "2343248"  # Price (6 digits)
+    fac_num = "2708"    
+    lot = "ebi233"      
+    exp_date = "0124" 
+    price = "2343248"  
+    Bc_data = fac_num + lot + exp_date + price
+    fish = "med_barcode3"
+    pambcgen(Bc_data, fish)
 
-    # Concatenate variables into a single string
-    barcode_data = fac_num + lot + exp_date + price
-
-    # Generate barcode image
-    filename = "product_barcode3"
-    generate_barcode(barcode_data, filename)
-
-    print(f"Barcode generated successfully: {filename}")
+    print(f"Barcode generated successfully: {fish}")
